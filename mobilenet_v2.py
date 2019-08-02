cc67f0cce8c8b6618a3804d8990cc2af93fdec60
M. Yusuf Sarıgöz <yusufsarigoz@gmail.comexit>
2019-08-02 22:33:58 +0300

    Initial release

---
+++ mobilenet_v2.py
@@ -0,0 +1,136 @@
+﻿"""
+# Reference
+- [Inverted Residuals and Linear Bottlenecks Mobile Networks for
+   Classification, Detection and Segmentation]
+   (https://arxiv.org/abs/1801.04381)
+"""
+
+import tensorflow as tf
+
+# Define ReLU6 activation
+relu6 = tf.keras.layers.ReLU(6.)
+
+def _conv_block(inputs, filters, kernel, strides):
+    """Convolution Block
+    This function defines a 2D convolution operation with BN and relu6.
+
+    # Arguments
+        inputs: Tensor, input tensor of conv layer.
+        filters: Integer, the dimensionality of the output space.
+        kernel: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+        strides: An integer or tuple/list of 2 integers,
+            specifying the strides of the convolution along the width and height.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+
+    # Returns
+        Output tensor.
+    """
+
+    x = tf.keras.layers.Conv2D(filters, kernel, padding='same', strides=strides)(inputs)
+    x = tf.keras.layers.BatchNormalization()(x)
+    return relu6(x)
+
+
+def _bottleneck(inputs, filters, kernel, t, s, r=False):
+    """Bottleneck
+    This function defines a basic bottleneck structure.
+
+    # Arguments
+        inputs: Tensor, input tensor of conv layer.
+        filters: Integer, the dimensionality of the output space.
+        kernel: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+        t: Integer, expansion factor.
+            t is always applied to the input size.
+        s: An integer or tuple/list of 2 integers,specifying the strides
+            of the convolution along the width and height.Can be a single
+            integer to specify the same value for all spatial dimensions.
+        r: Boolean, Whether to use the residuals.
+
+    # Returns
+        Output tensor.
+    """
+
+    tchannel = inputs.shape[-1] * t
+
+    x = _conv_block(inputs, tchannel, (1, 1), (1, 1))
+
+    x = tf.keras.layers.DepthwiseConv2D(kernel, strides=(s, s), depth_multiplier=1, padding='same')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = relu6(x)
+
+    x = tf.keras.layers.Conv2D(filters, (1, 1), strides=(1, 1), padding='same')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+
+    if r:
+        x = tf.keras.layers.add([x, inputs])
+    return x
+
+
+def _inverted_residual_block(inputs, filters, kernel, t, strides, n):
+    """Inverted Residual Block
+    This function defines a sequence of 1 or more identical layers.
+
+    # Arguments
+        inputs: Tensor, input tensor of conv layer.
+        filters: Integer, the dimensionality of the output space.
+        kernel: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+        t: Integer, expansion factor.
+            t is always applied to the input size.
+        s: An integer or tuple/list of 2 integers,specifying the strides
+            of the convolution along the width and height.Can be a single
+            integer to specify the same value for all spatial dimensions.
+        n: Integer, layer repeat times.
+    # Returns
+        Output tensor.
+    """
+
+    x = _bottleneck(inputs, filters, kernel, t, strides)
+
+    for i in range(1, n):
+        x = _bottleneck(x, filters, kernel, t, 1, True)
+
+    return x
+
+
+def MobileNetv2(input_shape, k, plot_model=False):
+    """MobileNetv2
+    This function defines a MobileNetv2 architecture.
+
+    # Arguments
+        input_shape: An integer or tuple/list of 3 integers, shape
+            of input tensor.
+        k: Integer, number of classes.
+        plot_model: Boolean, whether to plot model architecture or not
+    # Returns
+        MobileNetv2 model.
+    """
+
+    inputs = tf.keras.layers.Input(shape=input_shape, name='input')
+    x = _conv_block(inputs, 32, (3, 3), strides=(2, 2))
+
+    x = _inverted_residual_block(x, 16, (3, 3), t=1, strides=1, n=1)
+    x = _inverted_residual_block(x, 24, (3, 3), t=6, strides=2, n=2)
+    x = _inverted_residual_block(x, 32, (3, 3), t=6, strides=2, n=3)
+    x = _inverted_residual_block(x, 64, (3, 3), t=6, strides=2, n=4)
+    x = _inverted_residual_block(x, 96, (3, 3), t=6, strides=1, n=3)
+    x = _inverted_residual_block(x, 160, (3, 3), t=6, strides=2, n=3)
+    x = _inverted_residual_block(x, 320, (3, 3), t=6, strides=1, n=1)
+
+    x = _conv_block(x, 1280, (1, 1), strides=(1, 1))
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    x = tf.keras.layers.Reshape((1, 1, 1280))(x)
+    x = tf.keras.layers.Dropout(0.3, name='Dropout')(x)
+    x = tf.keras.layers.Conv2D(k, (1, 1), padding='same')(x)
+    x = tf.keras.layers.Activation('softmax', name='final_activation')(x)
+    output = tf.keras.layers.Reshape((k,), name='output')(x)
+    model = tf.keras.models.Model(inputs, output)
+    model.summary()
+    if plot_model:
+        tf.keras.utils.plot_model(model, to_file='model.png', show_shapes=True)
+
+    return model
+
---
+++ readme.md
@@ -0,0 +1,35 @@
+# mobilenetv2-tf2
+A TensorFlow 2.0 implementation of [Inverted Residuals and Linear Bottlenecks Mobile Networks for
+   Classification, Detection and Segmentation](https://arxiv.org/abs/1801.04381), aka MobileNetV2.
+
+## Motivation
+
+MobileNetV2  is still one of the most efficient architectures for image classification. Considering that TensorFlow 2.0 has already hit version beta1, I think that a flexible and reusable implementation of MobileNetV2 in TF 2.0 might be useful for practitioners.
+
+## Install
+
+1. `conda create -n mobilenetv2 python=3.6.8`
+2. `conda activate mobilenetv2`
+3. `git clone https://github.com/monatis/mobilenetv2-tf2.git`
+4. `cd mobilenetv2-tf2`
+5. `python -m pip install -r requirements.gpu.txt` # Change to `requirements.cpu.txt` if you're not using GPU.
+
+## Usage
+
+`train_dir` and `validation_dir` directories should contain a subdirectory for each class in the dataset. Then run:
+
+- `python train.py --train_dir /path/to/training/images --validation_dir /path/to/validation/images`
+- See `model/` directory for training output.
+
+run `python train.py --help` to see all the options.
+
+## Roadmap
+
+- [x] Share model architecture and a training script.
+- [x] Implement export to saved model.
+- [x] Implement command line arguments to configure data augmentation.
+- [ ] Share an inference script.
+- [ ] Implement mean and STD normalization.
+- Implement confusion matrix.
+- [ ] Implement export to TFLite for model inference.
+- [ ] Share an example Android app using the exported TFLite model.
